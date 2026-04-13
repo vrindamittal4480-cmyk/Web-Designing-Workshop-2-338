cd8e678690aca74b13e6b785c30026ed27c139ca
vrindamittal4480-cmyk <vrindamittal4480@gmail.com>
2026-04-13 07:05:01 +0530

    js fundamentals

---
+++ app.js
@@ -0,0 +1,17 @@
+var name ="Vrinda Mittal";
+var age =19;
+var isEmployed= true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+var age= 40;
+console.log(age);
+let greeting = "Hello, " +name + "You are " + age + "years old."
+console.log(greeting);
+let message = `hello $(name), you are $(age) years onload.` ;
+console.log(message);
+
+// if(true){
+//     let age =30;
+// }
+//    console.log(age);
