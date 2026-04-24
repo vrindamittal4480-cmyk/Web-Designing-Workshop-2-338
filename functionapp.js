6647ee93a782aaf30d409c8586d33b1433fb35f6
vrindamittal4480-cmyk <vrindamittal4480@gmail.com>
2026-04-24 13:54:04 +0530

    function

---
+++ functionapp.js
@@ -0,0 +1,31 @@
+let divide = function(a,b){
+    return a/b;
+};
+console.log(divide(4,2));
+console.log(divide(4,5));
+console.log(divide(4,3));
+console.log(divide(4,2.2));
+
+let add = function(a,b){
+    return a+b;
+};
+console.log(add(4,2));
+console.log(add(4,5));
+console.log(add(4,3));
+console.log(add(4,2.2));
+
+let sub = function(a,b){
+    return a-b;
+};
+console.log(sub(4,2));
+console.log(sub(4,5));
+console.log(sub(4,3));
+console.log(sub(4,2.2));
+
+let mul = function(a,b){
+    return a*b;
+};
+console.log(add(4,2));
+console.log(sub(4,5));
+console.log(mul(4,3));
+console.log(divide(4,2.2));
