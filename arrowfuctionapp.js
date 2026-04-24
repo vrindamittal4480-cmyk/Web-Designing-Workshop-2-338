3765fc7256b3bf9ce2d4e4a173394cbc17b720ba
vrindamittal4480-cmyk <vrindamittal4480@gmail.com>
2026-04-24 13:51:42 +0530

    functions

---
+++ arrowfuctionapp.js
@@ -0,0 +1,11 @@
+//arrow function with single parameter
+let square = x => x*x;
+console.log(square(4));
+
+//arrow function with one parameter
+const greetUser = (name) =>{
+    console.log('hello,${name}!');
+}
+greetUser("Alice");
+
+
